b2e0ee05fefeb80f098160be53ea991efedcef0c
Paul Dawson <excidere@gmail.com>
2013-01-18 16:28:02 -0600

    created group class

---
+++ lib/aok_scraper/group.rb
@@ -0,0 +1,14 @@
+module AokScraper
+  class Group
+    def initialize(name, url)
+      @name = name
+      @url  = url
+      @membership = {}
+    end
+
+    def add_member_count(date, member_count)
+      @membership[date] = member_count
+    end
+
+  end
+end
